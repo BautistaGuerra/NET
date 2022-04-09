939496e049632e4498a3bfe6babfdc2a86de9659
Bautista Guerra <bauguerra@gmail.com>
2022-04-09 16:17:40 -0300

    laboratorio 1.2 terminado

---
+++ Lab02/Lab02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor_1 = "Este es el valor 1";
+            int valor_2 = 5;
+            string valor_3 = valor_1;
+
+            Console.WriteLine(valor_1);
+            Console.WriteLine(valor_2);
+            Console.WriteLine(valor_3);
+            Console.ReadKey();
+
+        }
+    }
+}
